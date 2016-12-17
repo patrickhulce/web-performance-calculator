f388ff7bc898ae34a47e0eac371fe3680eecb7a1
Patrick Hulce <phulce@google.com>
2016-12-16 17:03:58 -0800

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ mv dist/ build/assets/
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git add -A
-git checkout -b __throwaway
+git checkout -b gh-pages
 git commit -m 'deploy to gh-pages'
 git subtree push --prefix build/ origin gh-pages
 git checkout -f $BRANCH
-git branch -D __throwaway
+git branch -D gh-pages
