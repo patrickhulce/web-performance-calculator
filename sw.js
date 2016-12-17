8199b9332ad7adcfa142f517692011a8b30d6973
Patrick Hulce <phulce@google.com>
2016-12-16 16:51:15 -0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+var VERSION = '1';
+
+this.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(VERSION).then(cache => {
+    return cache.addAll([
+      'index.html',
+      'assets/app.js',
+      'sw.js',
+    ]);
+  }))
+});
+
+this.addEventListener('fetch', function(e) {
+  var tryInCachesFirst = caches.open(VERSION).then(cache => {
+    return cache.match(e.request).then(response => {
+      return response || handleNoCacheMatch(e);
+    });
+  });
+  e.respondWith(tryInCachesFirst);
+});
+
+this.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.map(key => {
+      if (key !== VERSION)
+        return caches.delete(key);
+    }));
+  }));
+});
+
+function handleNoCacheMatch(e) {
+  return fetch(e.request).then(res => {
+    return caches.open(VERSION).then(cache => {
+      cache.put(e.request, res.clone());
+      return res;
+    });
+  });
+}
